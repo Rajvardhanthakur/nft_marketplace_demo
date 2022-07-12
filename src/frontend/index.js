3ed684cf1338d633057b2ba1ca8aeace9ac38558
Rajvardhanthakur <rajthakur3619@gmail.com>
2022-07-12 14:57:58 +0530

    initial commit for nft marketplace

---
+++ src/frontend/index.js
@@ -0,0 +1,6 @@
+import * as React from "react";
+import "bootstrap/dist/css/bootstrap";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
