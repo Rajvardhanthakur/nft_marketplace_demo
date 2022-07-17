193dd20d47bf96329e2661281b0bfa7b38a344cd
Rajvardhanthakur <rajthakur3619@gmail.com>
2022-07-18 01:38:15 +0530

    context created to moniter transactions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.css'
 import App from "./frontend/App";
+import { TransactionProvider } from "./frontend/context/TransactionContext";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<TransactionProvider><App /></TransactionProvider>, rootElement);
